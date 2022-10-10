911a2d9cd61ccc4b946a6b4074773edcfa76f7bb
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-10-10 13:50:49 +0200

    ejercicios actualizados

--- src/Controller/ContactoController.php
+++ src/Controller/ContactoController.php
@@ -5,7 +5,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use App\Entity\Contacto;
+use Doctrine\Persistence\ManagerRegistry;
+
 class ContactoController extends AbstractController{
+
+    #[Route('/contacto/insertar', name:"insertar_contacto")]
+    public function insertar(ManagerRegistry $doctrine){
+        $entityManager = $doctrine -> getManager();
+        foreach($this -> contactos as $c){
+            $contacto = new Contacto();
+            $contacto -> setNombre($c["nombre"]);
+            $contacto -> setTelefono($c["telefono"]);
+            $contacto -> setEmail($c["email"]);
+            $entityManager -> persist($contacto);
+        }try{
+            //Sólo se necesita realizar flush una vez
+            // y confirmará todas las operaciones pendientes
+            $entityManager->flush();
+            return new Response("Contactos insertados");
+        }catch(\Exception $e){
+            return new Response("Error insertando objetos");
+        }
+    }
+    
     private $contactos = [
 
         1 => ["nombre" => "Juan Pérez", "telefono" => "524142432", "email" => "juanp@ieselcaminas.org"],
@@ -20,9 +43,10 @@ class ContactoController extends AbstractController{
     ];
 
     #[Route('/contacto/{codigo}', name: 'ficha_contacto')]
-    public function ficha($codigo): Response{
-        $resultado = ($this -> contactos[$codigo] ?? null);
-        return $this -> render("ficha_contacto.html.twig",["contacto" => $resultado]);
+    public function ficha(ManagerRegistry $doctrine, $codigo): Response{
+        $repositorio = $doctrine -> getRepository(Contacto::class);
+        $contacto = $repositorio -> find($codigo);
+        return $this->render("ficha_contacto.html.twig",["contacto" => $contacto]);
     }
 
     #[Route('/contacto/buscar/{texto}', name:"buscar_contacto")]
@@ -35,6 +59,6 @@ class ContactoController extends AbstractController{
         );
         return $this->render("lista_contactos.html.twig",["contactos" => $resultados]);
     }
-    
+
 }
 ?>
